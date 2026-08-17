66d7fa13c45f1960cea3303f893727a15c15a531
Ben-Salem <salemko801@gmail.com>
2026-08-17 09:01:14 +0100

    Affichage des films

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-const apiKey = "840304608d90b96564a264a491917970";
-const apiUrl = "https://api.themoviedb.org/3";
-async function getPopularMovies() {
+    const apiKey = "840304608d90b96564a264a491917970";
+    const apiUrl = "https://api.themoviedb.org/3";
+    const moviesContainer = document.getElementById("moviesContainer");
 
-    try {
+    let movies = [];
+    async function getPopularMovies() {
 
-        const response = await fetch(
-            `${apiUrl}/movie/popular?api_key=${apiKey}&language=fr-FR`
-        );
+        try {
 
-        if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des films");
+            const response = await fetch(
+                `${apiUrl}/movie/popular?api_key=${apiKey}&language=fr-FR`
+            );
+
+            if (!response.ok) {
+                throw new Error("Erreur lors de la récupération des films");
+            }
+
+            const data = await response.json();
+
+            movies = data.results;
+
+            displayMovies(movies);
+        } catch (error) {
+
+            console.error(error);
+
         }
-
-        const data = await response.json();
-
-        console.log(data);
-
-    } catch (error) {
-
-        console.error(error);
-
     }
-}
 
-getPopularMovies();
+    getPopularMovies();
+
+    function displayMovies(moviesToDisplay) {
+
+        moviesContainer.innerHTML = "";
+
+        moviesToDisplay.forEach(movie => {
+
+            const movieCard = document.createElement("article");
+
+            movieCard.classList.add("movie-card");
+
+            movieCard.innerHTML = `
+                <img 
+                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+                    alt="${movie.title}"
+                >
+
+                <h2>${movie.title}</h2>
+
+                <p>${movie.release_date}</p>
+
+                <strong>${movie.vote_average.toFixed(1)}</strong>
+            `;
+
+            moviesContainer.appendChild(movieCard);
+        });
+    }
